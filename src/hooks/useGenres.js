ccf44cff49e035206ba91a3ec79fd1d8367200fb
Sharif Khan <sharifkhan74647464@gmail.com>
2024-07-27 19:29:33 +0600

    fetch genres

---
+++ src/hooks/useGenres.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import apiClient, { CanceledError } from "../services/api-client";
+
+const useGenres = () => {
+  const [genres, setGenres] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
