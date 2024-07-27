0b15855c737ba64afdd2817fbb8d084c8da8a7a2
Sharif Khan <sharifkhan74647464@gmail.com>
2024-07-27 16:04:55 +0600

    implement game card

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,14 @@
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+
+const GameCard = ({ game }) => {
+  return (
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
