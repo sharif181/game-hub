c0098704dee6912f9deb7b72928bb31733d6c8d7
Sharif Khan <sharifkhan74647464@gmail.com>
2024-07-27 19:32:33 +0600

    remove console log

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,7 +1,7 @@
 import useGenres from "../hooks/useGenres";
 const GenreList = () => {
   const { genres, error, isLoading } = useGenres();
-  console.log(genres);
+
   return (
     <ul>
       {genres.map((genre) => (
