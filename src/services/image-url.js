cab02a608f7a9f4fec9b233a64b9d270635faf1d
Sharif Khan <sharifkhan74647464@gmail.com>
2024-07-28 20:02:40 +0600

    handle game with no image

--- src/services/image-url.js
+++ src/services/image-url.js
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const keyword = "media/";
   const index = url.indexOf(keyword) + keyword.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
